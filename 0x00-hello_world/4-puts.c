e8ab9728f0b80ad76ca2cef69b6f2aa3560070df
Bhlak <bhlakcodes@proton.me>
2022-09-08 15:08:43 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
- char a[100] = "\"Programming is like building a multilingual puzzle \n";
+char a[100] = "\"Programming is like building a multilingual puzzle ";
 
- puts(a);
- return (0);
+puts(a);
+return (0);
 }
