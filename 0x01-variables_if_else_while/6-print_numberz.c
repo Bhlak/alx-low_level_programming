f48c3676dfb1f54b3580038c8c1c733d606f3247
Bhlak <bhlakcodes@proton.me>
2022-09-09 09:41:03 -0700

    ASCII printing

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Using ascii to print numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
