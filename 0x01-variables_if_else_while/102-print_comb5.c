9b78a2d650042d796fd9a90a26c4de96275c78e0
Bhlak <bhlakcodes@proton.me>
2022-09-09 11:59:08 -0700

    Eat the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,31 +11,30 @@ int main(void)
 {
 	int i, j, k, l;
 
-	for (i = 48; i < 58; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 48; j < 57; j++)
+		for (j = 0; j < 10; j++)
 		{
-			for (k = 48; k < 58; k++)
+			for (k = 0; k < 10; k++)
 			{
-				for (l = j + 1; l < 58; l++)
+				for (l = 0; l < 10; l++)
 				{
-					if (i >= k || j >= l)
+					if ((i > k || j > l) || (i == k && j == l))
 					{
 						continue;
 					}
-					putchar(i);
-					putchar(j);
+					putchar('0' + i);
+					putchar('0' + j);
 					putchar(' ');
-					putchar(k);
-					putchar(l);
-					if (i + j == 113 || k + l == 114)
+					putchar('0' + k);
+					putchar('0' + l);
+					if (!(i + j == 17 && k + l == 18))
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
 			}
-
 		}
 	}
 	putchar('\n');
