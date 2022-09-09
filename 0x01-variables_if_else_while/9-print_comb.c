8dd06c4753731a3598d4afc4bc1381c6903e0736
Bhlak <bhlakcodes@proton.me>
2022-09-09 10:17:14 -0700

    Combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,20 +10,17 @@
 int main(void)
 {
 	int i;
-	int j;
 
 	for (i = 48; i < 58; i++)
 	{
-		for (j = 48; j < 58; j++)
+		putchar(i);
+		if (i == 57)
 		{
-			putchar(i);
-			putchar(j);
-			if (i != 57 || j != 57)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			break;
 		}
+		putchar(',');
+		putchar(' ');
 	}
+	putchar('\n');
 	return (0);
 }
