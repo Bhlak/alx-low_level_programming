c910303ef6d95eaea7c9504faf698fe43e23d921
Bhlak <bhlakcodes@proton.me>
2022-09-13 06:26:08 -0700

    The game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Alwayxs 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+	while (a <= 'z')
+	{
+		_putchar(a++);
+	}
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
