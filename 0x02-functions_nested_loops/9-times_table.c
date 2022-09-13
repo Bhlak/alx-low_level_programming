25cc2c165b096a12d0382d33c88837fb67002d9b
Bhlak <bhlakcodes@proton.me>
2022-09-13 13:59:23 -0700

    9 times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,23 +9,31 @@ void times_table(void)
 	int i, j, res;
 
 	i = 0;
-
 	while (i < 10)
 	{
 		j = 0;
 		while (j < 10)
 		{
 			res = i * j;
-			_putchar(res + '0');
+			if (res < 10)
+			{
+				putchar((res % 10) + 48);
+			}
+			else
+			{
+				putchar((res / 10) + 48);
+				putchar((res % 10) + 48);
+			}
+
 			if (j == 9)
 			{
 				break;
 			}
 			j++;
-			_putchar(',');
-			_putchar(' ');
+			putchar(',');
+			putchar (' ');
 		}
-		_putchar('\n');
+		putchar('\n');
 		i++;
 	}
 }
