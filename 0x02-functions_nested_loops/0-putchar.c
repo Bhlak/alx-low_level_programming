8e8de2177ec021ab455d336f6d4a71283b8f9d45
Bhlak <bhlakcodes@proton.me>
2022-09-13 06:13:24 -0700

    Test

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Description - Program to print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
