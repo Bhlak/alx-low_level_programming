a0650c95a5d3b252a9c2e1dce13fc70883595e90
Bhlak <bhlakcodes@proton.me>
2022-09-13 10:23:39 -0700

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Finds the absolute value of a number
+ *
+ * @a: Number to be checked
+ *
+ * Return: Absolute value of the number
+ */
+
+int _abs(int a)
+{
+	return (n >= 0 ? n : n * -1);
+}
