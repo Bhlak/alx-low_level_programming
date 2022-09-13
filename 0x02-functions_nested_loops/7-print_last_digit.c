e7c58b4704db4d217de6582cc89ca9fc466c2f3e
Bhlak <bhlakcodes@proton.me>
2022-09-13 10:33:15 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ *
+ * @a: Number whose digit is to be displayed
+ *
+ * Return: Always the last digit of the number given
+ */
+
+int print_last_digit(int a)
+{
+	return (a % 10);
+}
