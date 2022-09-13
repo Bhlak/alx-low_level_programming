781a55828dc0b77919deaf0929faec842644337c
Bhlak <bhlakcodes@proton.me>
2022-09-13 15:26:52 -0700

    Sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ *
+ * @x: first integer to be summed
+ *
+ * @y: Second integer to be summed
+ *
+ * Return: Returns sum of values
+ */
+
+int add(int x, int y)
+{
+	return(x + y);
+	
+}
