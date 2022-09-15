8a4fbb9fcc754e446c077e9e5a35639ecb592aee
Bhlak <bhlakcodes@proton.me>
2022-09-15 15:06:30 -0700

    Uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,4 +2,15 @@
 #include <stdio.h>
 
 /**
- * main - check
+ * _isupper - checks for an uppercase character
+ * @c: Character to be checked
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
