7e4fb56a5934762384a5f8cf1cedf4b79c33499f
Bhlak <bhlakcodes@proton.me>
2022-09-15 16:56:55 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 1;
+
+	while (a < 101)
+	{
+		if (a % 3 == 0)
+		{
+			if (a % 5 == 0)
+			{
+				printf("FizzBuzz");
+			}
+			else
+			{
+				printf("Fizz");
+			}
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz")
+		}
+		else
+		{
+			printf("%d", a)
+		}
+
+		if (a != 100)
+		{
+			printf(" ");
+		}
+		a++;
+	}
+	putchar('\n');
+}
