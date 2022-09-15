cdbe78e0f31dfe5d5ee637178b7d8520d1ae9efa
Bhlak <bhlakcodes@proton.me>
2022-09-15 15:38:43 -0700

    Printing most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints most numbers between 0 and 9
+ */
+
+void print_most_numbers(void)
+{
+	int a = 48;
+
+	while (a < 58)
+	{
+		if (a == 50 || a == 52)
+		{
+			continue;
+		}
+		_putchar(a);
+	}
+	_putchar('\n');
+}
