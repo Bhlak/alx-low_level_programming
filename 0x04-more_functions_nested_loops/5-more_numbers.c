dea19fbdd219c2ba1115a09dd363502ae1842da8
Bhlak <bhlakcodes@proton.me>
2022-09-15 15:59:00 -0700

    More

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers from 0 - 14 x 10
+ */
+
+void more_numbers(void)
+{
+	int a = 0;
+
+	for (int i = 0; i < 10; i++)
+	{
+		while (a < 15)
+		{
+			if (a > 9)
+			{
+				_putchar(a / 10 + '0');
+			}
+			_putchar(a % 10 + '0');
+			a++;
+		}
+		_putchar('\n');
+	}
+}
