5e5c79cbc9d72545de3df2e9f71f7c3df66d360e
Bhlak <bhlakcodes@proton.me>
2022-09-15 17:41:14 -0700

    Triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,17 +10,24 @@ void print_triangle(int size)
 	int a, b, space;
 	int shape = 1;
 
-	for (a = 0; a < size; a++)
+	if (size <= 0)
 	{
-		for (space = size - shape; space > 0; space--)
-		{
-			_putchar(' ');
-		}
-		for (b = 0; b < shape; b++)
-		{
-			_putchar('#');
-		}
-		shape++;
 		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (space = size - shape; space > 0; space--)
+			{
+				_putchar(' ');
+			}
+			for (b = 0; b < shape; b++)
+			{
+				_putchar('#');
+			}
+			shape++;
+			_putchar('\n');
+		}
 	}
 }
