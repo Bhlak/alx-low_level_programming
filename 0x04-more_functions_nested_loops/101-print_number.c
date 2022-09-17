40fce318629eabd1a8f323b2d4df11b2e725ff1f
Bhlak <bhlakcodes@proton.me>
2022-09-17 01:36:15 -0700

    Late

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,6 +7,8 @@
 
 void print_number(int n)
 {
+	unsigned int m;
+	
 	if (n >= 0)
 	{
 		if (n >= 10)
@@ -17,13 +19,13 @@ void print_number(int n)
 	}
 	else
 	{
-		n = -1 * n;
+		m = -1 * n;
 
 		_putchar('-');
-		if (n >= 10)
+		if (m >= 10)
 		{
-			print_number(n / 10);
+			print_number(m / 10);
 		}
-		_putchar(n % 10 + '0');
+		_putchar(m % 10 + '0');
 	}
 }
