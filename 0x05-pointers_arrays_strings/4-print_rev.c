d27843b27ef95883024717978f2c103299a07ed9
Bhlak <bhlakcodes@proton.me>
2022-09-20 09:06:13 -0700

    Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: String to be reversed
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		_puthar(s[i++]);
+	}
+	_putchar('\n');
+}
