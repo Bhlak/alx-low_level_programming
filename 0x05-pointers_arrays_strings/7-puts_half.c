49d277d58f9228857d5ae482c978d2d6c62c276d
Bhlak <bhlakcodes@proton.me>
2022-09-22 16:29:06 -0700

    Batch

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @str: String to be processed
+ */
+
+void puts_half(char *str)
+{
+	int count = 0, i;
+
+	while (*(str + count) != '\0')
+	{
+		count++;
+	}
+	if (count % 2 == 0)
+	{
+		
+	}
+}
