d93fa3d9d2f73fd15e9acb3f7555fd769254ba91
Bhlak <bhlakcodes@proton.me>
2022-09-22 10:59:35 -0700

    Copier

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strncpy - Copies a string
+ * @dest: Target string
+ * @src: String to be copied
+ * @n: Integer count
+ * Return: Pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i++];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
