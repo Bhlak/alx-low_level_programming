c41796660ee64a69678527ac72583c45db7a6c55
Bhlak <bhlakcodes@proton.me>
2022-09-22 16:38:51 -0700

    Capitalizeee

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,22 +10,23 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
+	if (str[0] <= 122 && str[0] >= 97)
+	{
+		str[0] = str[0] - 32;
+	}
 	while (str[i] != '\0')
 	{
-		if (!((str[i] > 64 && str[i] < 91) || (str[i] > 96 && str[i] < 123)))
+		if (str[i] == 32 || str[i] == 46 || str[i] == '\t' ||
+		    str[i] == '\n' || str[i] == 44 || str[i] == 59 ||
+		    str[i] == '!' || str[i] == '?' || str[i] == '(' ||
+		    str[i] == ')' || str[i] == '{' || str[i] == '}')
 		{
-			if (str[i + 1] > 96 && str[i + 1] < 123)
+			if (str[i + 1] <= 122 && str[i + 1] >= 97)
 			{
-				if (!(str[i] > 47 && str[i] < 58))
-				{
-					if (!(str[i] > 47 && str[i] < 58))
-					{
-						str[i + 1] = str[i + 1] - 32;
-					}
-				}
+				str[i + 1] = str[i + 1] - 32;
 			}
 		}
-		i++;
+	i++;
 	}
 	return (str);
 }
