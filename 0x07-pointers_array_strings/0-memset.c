332ed937b883c29574a7444592158e7bb6ca8338
Bhlak <bhlakcodes@proton.me>
2022-09-27 12:40:02 -0700

    Automated Commit

---
+++ 0x07-pointers_array_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memset - Fills memory with a constant byte
+ * @s: String to be filled
+ * @b: Byte to be inputed
+ * @n: First n bytes
+ * Return: pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int a = 0;
+
+	while (a < n)
+	{
+		s[a] = b;
+		a++;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_array_strings/README.md
@@ -0,0 +1,3 @@
+# Even More Pointers, Arrays and Strings
+
+Directory made for the above stated project of the ALX SE course
