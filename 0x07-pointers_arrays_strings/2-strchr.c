73bb94dd69206d173e9e6ccc3e14e87503a5be66
Bhlak <bhlakcodes@proton.me>
2022-09-27 13:03:22 -0700

    Automated Commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strchr - Locates a character in a string
+ * @s: String
+ * @c: Character to be found
+ * Return: pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a = 0;
+	char *ptr;
+
+	while (s[a] != c)
+	{
+		a++;
+	}
+	ptr = &s[a];
+
+	return (ptr);
+}
