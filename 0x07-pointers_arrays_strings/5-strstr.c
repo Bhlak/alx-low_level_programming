8af238d7f0b9c3c526379799201b02b3acf5a21b
Bhlak <bhlakcodes@proton.me>
2022-09-27 16:10:20 -0700

    Automated Commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring
+ * @haystack: String to be processed
+ * @needle: Substring to be located
+ * Return: pointer or null
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int a = 0, count = 0;
+
+	while (needle[a])
+	{
+		count++;
+		a++;
+	}
+
+	while (*haystack)
+	{
+		if (*haystack == *needle && *(haystack + count) == *(needle + count))
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return (0);
+}
