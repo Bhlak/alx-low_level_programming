73b4a6419b49610d6133abc34a1df350a3db74dc
Bhlak <bhlakcodes@proton.me>
2022-09-28 15:16:43 -0700

    Batch Recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - !
+ * @n: Number to be processed
+ * Return: integer
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n + factorial(n - 1));
+	}
+}
