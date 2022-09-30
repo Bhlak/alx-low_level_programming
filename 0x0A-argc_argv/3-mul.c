6dfa08954375e594ae6aebd01100a582b9b6149e
Bhlak <bhlakcodes@proton.me>
2022-09-30 09:20:54 -0700

    Automated Commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiplies two arguments
+ * @argc: Number of arguments
+ * @argv: Array of strings
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int a, b;
+
+	if (argc < 3)
+	{
+		puts("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	printf("%d\n", a * b);
+	return (0);
+}
