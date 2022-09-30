530c57b0b269e4d038230de81666ab0b6adad5bd
Bhlak <bhlakcodes@proton.me>
2022-09-30 12:59:58 -0700

    Automated Commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Adds two arguments
+ * @argc: Number of arguments
+ * @argv: Array of strings(arguments)
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
