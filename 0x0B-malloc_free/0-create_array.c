21247e135b98eebe87f3bc70c671494b2b5b5702
Bhlak <bhlakcodes@proton.me>
2022-10-03 10:24:27 -0700

    Automated Commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -21,7 +21,7 @@ char *create_array(unsigned int size, char c)
 	ar = malloc(sizeof(char) *  size);
 	if (ar == NULL)
 		return (NULL);
-	for (i = 0; ar[i]; i++)
+	for (i = 0; i < size; i++)
 	{
 		ar[i] = c;
 	}
