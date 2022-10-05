7218fbea13da14b294310298f69942be751db3fd
Bhlak <bhlakcodes@proton.me>
2022-10-05 05:29:01 -0700

    Automated Commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,11 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 	unsigned int i = 0, j = 0, k = 0, l = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[i++] != '\0')
 		;
 	while (s2[j++] != '\0')
