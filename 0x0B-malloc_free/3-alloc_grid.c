c3879250e0566385010d7da90fdce0cfd900939c
Bhlak <bhlakcodes@proton.me>
2022-10-05 06:49:54 -0700

    Automated Commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,8 +22,7 @@ int **alloc_grid(int width, int height)
 		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
 		{
-			i--;
-			for (i; i >= 0; i--)
+			for (i--; i >= 0; i--)
 				free(arr[i]);
 			return (NULL);
 		}
