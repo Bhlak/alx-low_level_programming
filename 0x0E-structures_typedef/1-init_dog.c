98efe218753364146140db7babedbdcd914b5dc1
Bhlak <bhlakcodes@proton.me>
2022-10-10 13:43:33 -0700

    Automated Commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - Function to initialize a variable of type struct dog
+ * @d: Dog object
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ */
+
+void init_dog(struct dog *d, char *name, float *age, char *owner)
+{
+	if (d != NULL)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+}
