f1c4e65fbe70b4e23ca9c87ba18757b467bd12e9
Bhlak <bhlakcodes@proton.me>
2022-10-10 07:57:37 -0700

    Automated Commit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+# Structures, Typedef
+
+Directory made for the Structures and Typedef project of the Alx Low Level Programming Section of the Software Engineering course
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - Structure of a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: Details of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
