f6d04177ae7f9b529c65f16dbcb972f70a6e10bf
Bhlak <bhlakcodes@proton.me>
2022-10-10 14:33:12 -0700

    Automated Commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,9 +12,9 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n", (d->name)  ? d->name : "(nil\n)");
-		printf("Age: %f\n", (d->age) ? d->age : "(nil\n)");
-		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil\n)");
+		printf("Name: %s\n", (d->name)  ? (d->name) : "(nil)\n");
+		printf("Age: %f\n", (d->age) ? d->age : "(nil)\n");
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)\n");
 	}
 }
 
