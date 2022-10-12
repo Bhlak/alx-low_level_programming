5d34d6f9580e45337978295ed28702bcc0c772ed
Bhlak <bhlakcodes@proton.me>
2022-10-12 16:33:44 -0700

    Automated Func_Pointers Commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Executes a function on each element of an array
+ * @array: Array to be processed
+ * @size: Size of array
+ * @action: Function to be used
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (array == NULL || size == NULL || action == NULL)
+		return;
+	while (i < size)
+	{
+		action(array[i++]);
+	}
+}
