08d84b5f7ed37ba39ae5edb8c9cf71c2fa841c10
Bhlak <bhlakcodes@proton.me>
2022-10-12 16:49:33 -0700

    Automated Func_Pointers Commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+/**
+ * int_index -Searcheds for an integer
+ * @array: Array to be processed
+ * @size: Size of array
+ * @cmp: Function to be used
+ * Return: Index of integer
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i = 0;
+	int res;
+
+	if (size <= 0)
+		return (-1);
+	if (array && cmp)
+	{
+		while (i < size)
+		{
+			res = cmp(array[i]);
+			if (res)
+				return (i);
+			i++;
+		}
+		return (-1);
+	}
+}
