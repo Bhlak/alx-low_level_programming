c67a0a15472f7dc5fdcc36e0127a5b044c65f61c
Bhlak <bhlakcodes@proton.me>
2022-10-12 18:39:41 -0700

    Automated Func_Pointers Commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,6 +16,11 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
+	if ((argv[2] == '/') || (argv[2] == '%') && argv[3] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	oprt = get_op_func(argv[2]);
 	if (!oprt)
 	{
