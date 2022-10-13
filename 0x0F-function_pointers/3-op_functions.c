f56ca6c6888608919a0592e02ccb806455105e08
Bhlak <bhlakcodes@proton.me>
2022-10-12 18:06:11 -0700

    Automated Func_Pointers Commit

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Addition
+ * @a: First integer
+ * @b: Second integer
+ * Return: Sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtraction
+ * @a: First integer
+ * @b: Second integer
+ * Return: Difference
+ */
+
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * op_mul - Multiplication
+ * @a: First integer
+ * @b: Second integer
+ * Return: Product
+ */
+
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * op_div - Division
+ * @a: First integer
+ * @b: Second integer
+ * Return: Quotient
+ */
+
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * op_mod - Modulus
+ * @a: First integer
+ * @b: Second integer
+ * Return: Remainder
+ */
+
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
