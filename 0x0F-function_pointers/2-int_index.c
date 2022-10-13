45f1d97c955e9bfabbbe77daf2fc0a4f2b165833
Bhlak <bhlakcodes@proton.me>
2022-10-12 17:09:49 -0700

    Automated Func_Pointers Commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,12 +10,13 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	unsigned int i = 0;
+	int i = 0;
 
-	if (size <= 0)
-		return (-1);
 	if (array && cmp)
 	{
+		if (size <= 0)
+			return (-1);
+
 		while (i < size)
 		{
 			if (cmp(array[i]) != 0)
