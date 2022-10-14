a3a3add781e9e4b2def713439da10a7cc88e05a4
Bhlak <bhlakcodes@proton.me>
2022-10-14 10:40:38 -0700

    Variadic Commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variable_functions.h"
+
+/**
+ * print_numbers - Prints numbers folowed by a new line
+ * @separator: Separator between numbers
+ * @n: Number of parameters
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+	if (n == 0)
+		return (0);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator && i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
