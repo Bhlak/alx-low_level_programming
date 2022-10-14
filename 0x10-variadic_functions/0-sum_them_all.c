24f0c8bfa672db95632823f603523c7873476bef
Bhlak <bhlakcodes@proton.me>
2022-10-14 10:10:56 -0700

    Variadic Commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,6 @@
 /**
  * sum_them_all - Sums up parameters
  * @n: Number of integers
- * @...: Variable number of parameters
  * Return: Sum of parameters
  */
 
@@ -12,10 +11,10 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	va_list args;
 
-	va_start(args, n);
 	if (n == 0)
 		return (0);
 
+	va_start(args, n);
 	for (int i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
