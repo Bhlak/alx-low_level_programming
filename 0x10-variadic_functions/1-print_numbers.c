088840d7377f00e47a7075adfc3dba2397b73e99
Bhlak <bhlakcodes@proton.me>
2022-10-14 10:48:10 -0700

    Variadic Commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,8 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(args, n);
-	if (n == 0)
-		return (0);
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
