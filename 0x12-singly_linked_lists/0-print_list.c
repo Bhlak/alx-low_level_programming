abcfb90c9a147e00604d1c07888f7dac08770321
Bhlak <bhlakcodes@proton.me>
2022-10-24 13:24:57 -0700

    Automated Commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - Prints values of all nodes in a linked list
+ * @h: Head node
+ * Return: Number of Nodes
+ */
+
+size_t print_list(const list_h *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		if (!h.str)
+			printf("[0] (nil)");
+		else
+			printf("[%u] %s", i, h.str);
+		h = h.next;
+		i++;
+	}
+	return (i);
+}
