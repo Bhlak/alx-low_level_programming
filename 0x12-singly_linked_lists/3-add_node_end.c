848189327a593c3d52d5aee590fe5f99772bebc8
Bhlak <bhlakcodes@proton.me>
2022-10-26 03:37:19 -0700

    Automated Commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -18,7 +18,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new->str = strdup(str);
 	if (!new || !str || new->str == NULL)
 	{
-		free (new);
+		free(new);
 		return (NULL);
 	}
 	new->len = len;
