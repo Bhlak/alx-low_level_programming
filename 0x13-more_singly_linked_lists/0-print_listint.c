60a823cacaad87f0afcc6583dd73232be9995c05
Bhlak <bhlakcodes@proton.me>
2022-10-26 04:49:12 -0700

    Automated Commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - Prints all elements of a list
+ * @h: Head node
+ * Return: Number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+	listint_t *tmp = h;
+
+	if (!tmp)
+		return (NULL);
+	while (tmp)
+	{
+		printf("%d\n", tmp->n);
+		i++;
+		tmp = tmp->next;
+	}
+	return (i);
+}
