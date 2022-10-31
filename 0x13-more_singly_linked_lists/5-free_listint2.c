0d0faf7fbb8459e4974039c6f05719bec43c5a3b
Bhlak <bhlakcodes@proton.me>
2022-10-31 00:01:13 -0700

    Automated Commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,14 +9,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (!(*head))
-		return;
-
 	while (*head)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
-	*head = NULL;
 }
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Removes a head node
+ * @head: Head node
+ * Return: Head Node's n
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int res;
+
+	if (!(*head))
+		return (0);
+	tmp = (*head)->next;
+	res = (*head)->n;
+	free(*head);
+	*head = tmp;
+	return(res);
+}
