bf1a9373a6a9344be82c7d66f049725373d2c581
Bhlak <bhlakcodes@proton.me>
2022-11-01 06:53:33 -0700

    Automated Commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cret=ate_file - Creates a file
+ * @filename: File Name
+ * @text_content: Text to be written to file
+ *
+ * Return: 1 - On Success
+ * -1 - On Failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int o, r, len = 0;
+
+	if (!filename)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+	}
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+	return (1);
+}
