9e0c0f06108c432629a670ceda0f0923c7880d86
Bhlak <bhlakcodes@proton.me>
2022-11-01 09:04:52 -0700

    Automated Commit

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,90 @@
+#include "main.h"
+
+/**
+ * create_buffer - Creates a buffer
+ * @file: File to be written to
+ *
+ * Return: Buffer pointer
+ */
+
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+	if (!buffer)
+	{
+		free(buffer);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return (buffer);
+}
+
+/**
+ * close_file - Closes an opened file
+ * @fd: File descriptor for open file
+ */
+
+void close_file(int fd)
+{
+	int i;
+
+	i = close(fd);
+	if (i == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n". fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments passed
+ * @argv: Array of pointers to arguments passed
+ *
+ * Return: 0 - Upon Success
+ */
+
+int main(int argc, char **argv)
+{
+	int from, to, r, w;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+
+	while (r > 0)
+	{
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprint(STDERR_FILENO,
+			       "Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+	}
+	free(buffer);
+	close_file(from);
+	close_file(to);
+
+	return (0);
+}
